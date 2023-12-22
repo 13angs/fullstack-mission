d36076103834dd785e387907e8195f35595b7701
13angs <13angz.um@gmail.com>
2023-12-22 10:41:15 +0000

    add user authentication service

--- api/chat-sv/Controllers/ChatController.cs
+++ api/chat-sv/Controllers/ChatController.cs
@@ -1,6 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 using chat_sv.Hubs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using Newtonsoft.Json;
@@ -11,43 +21,47 @@ namespace chat_sv.Controllers
     [Route("api/chat")]
     public class ChatController : ControllerBase
     {
-        private readonly IMongoCollection<User> _usersCollection;  // Updated variable name
+        private readonly IMongoCollection<User> _usersCollection;
         private readonly IMongoCollection<Message> _messagesCollection;
         private readonly IHubContext<ChatHub> _chatHubContext;
+        private readonly IConfiguration _configuration;
 
-        public ChatController(IMongoClient mongoClient, IHubContext<ChatHub> chatHubContext)
+        public ChatController(IMongoClient mongoClient, IHubContext<ChatHub> chatHubContext, IConfiguration configuration)
         {
             var database = mongoClient.GetDatabase("chat_db");
-            _usersCollection = database.GetCollection<User>("users");  // Updated collection name
+            _usersCollection = database.GetCollection<User>("users");
             _messagesCollection = database.GetCollection<Message>("messages");
             _chatHubContext = chatHubContext;
+            _configuration = configuration;
         }
 
-        [HttpGet("users")]  // Updated endpoint
+        [HttpGet("users")]
+        [Authorize]
         public ActionResult<IEnumerable<User>> GetUsers()
         {
-            var users = _usersCollection.Find(user => true).ToList();  // Updated variable name
+            var users = _usersCollection.Find(user => true).ToList();
             return Ok(users);
         }
 
         [HttpPost("sendMessage")]
+        [Authorize]
         public async Task<ActionResult> SendMessage(MessageRequest messageRequest)
         {
-            if (messageRequest == null || string.IsNullOrWhiteSpace(messageRequest.Text) || string.IsNullOrEmpty(messageRequest.UserId))  // Updated property name
+            if (messageRequest == null || string.IsNullOrWhiteSpace(messageRequest.Text) || string.IsNullOrEmpty(messageRequest.UserId))
             {
                 return BadRequest("Invalid message request");
             }
 
-            User selectedUser = await _usersCollection.Find(u => u.Id == messageRequest.UserId).FirstOrDefaultAsync();  // Updated variable name
+            User selectedUser = await _usersCollection.Find(u => u.Id == messageRequest.UserId).FirstOrDefaultAsync();
 
             if (selectedUser == null)
             {
-                return NotFound("User not found");  // Updated message
+                return NotFound("User not found");
             }
 
             var newMessage = new Message
             {
-                UserId = selectedUser.Id,  // Updated property name
+                UserId = selectedUser.Id,
                 Text = messageRequest.Text,
                 Timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()
             };
@@ -60,30 +74,100 @@ namespace chat_sv.Controllers
         }
 
         [HttpGet("messages")]
+        [Authorize]
         public ActionResult<IEnumerable<Message>> GetUserMessages([FromQuery] MessageParams messageParams)
         {
-            if (string.IsNullOrEmpty(messageParams.UserId))  // Updated property name
+            if (string.IsNullOrEmpty(messageParams.UserId))
             {
                 var messages = _messagesCollection.Find(message => true).ToList();
                 return Ok(messages);
             }
 
-            var userMessages = _messagesCollection.Find(message => message.UserId == messageParams.UserId).ToList();  // Updated property name
+            var userMessages = _messagesCollection.Find(message => message.UserId == messageParams.UserId).ToList();
 
             if (userMessages == null || userMessages.Count == 0)
             {
-                return NotFound("No messages found for the specified user");  // Updated message
+                return NotFound("No messages found for the specified user");
             }
 
             return Ok(userMessages);
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginRequest loginRequest)
+        {
+            var user = await AuthenticateUserAsync(loginRequest.Username, loginRequest.Password);
+            if (user == null)
+            {
+                return Unauthorized("Invalid username or password");
+            }
+
+            var token = GenerateJwtToken(user);
+            return Ok(token);
+        }
+
+        private async Task<User> AuthenticateUserAsync(string username, string password)
+        {
+            var user = await _usersCollection.Find(u => u.Username == username).FirstOrDefaultAsync();
+
+            if (user != null && VerifyPassword(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return user;
+            }
+
+            return null;
+        }
+
+        private bool VerifyPassword(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            using (var hmac = new HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != storedHash[i])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"]!);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id),
+                    // Add additional claims if needed
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 
-    public class User  // Updated class name
+    public class User
     {
-        [BsonElement("_id")]
         [JsonProperty("_id")]
-        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Id { get; set; }
+
+        [BsonElement("username")]
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [BsonElement("password_hash")]
+        [JsonProperty("password_hash")]
+        public byte[] PasswordHash { get; set; }
+
+        [BsonElement("password_salt")]
+        [JsonProperty("password_salt")]
+        public byte[] PasswordSalt { get; set; }
 
         [BsonElement("name")]
         [JsonProperty("name")]
@@ -96,13 +180,12 @@ namespace chat_sv.Controllers
 
     public class Message
     {
-        [BsonElement("_id")]
         [JsonProperty("_id")]
-        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Id { get; set; }
 
-        [BsonElement("user_id")]  // Updated property name
+        [BsonElement("user_id")]
         [JsonProperty("user_id")]
-        public string UserId { get; set; } = string.Empty;
+        public string UserId { get; set; }
 
         [BsonElement("text")]
         [JsonProperty("text")]
@@ -115,18 +198,25 @@ namespace chat_sv.Controllers
 
     public class MessageRequest
     {
-        [BsonElement("user_id")]  // Updated property name
         [JsonProperty("user_id")]
         public string UserId { get; set; } = string.Empty;
 
-        [BsonElement("text")]
         [JsonProperty("text")]
         public string? Text { get; set; }
     }
 
     public class MessageParams
     {
-        [FromQuery(Name = "user_id")]  // Updated property name
+        [FromQuery(Name = "user_id")]
         public string UserId { get; set; } = string.Empty;
+    }
+
+    public class LoginRequest
+    {
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
     }
 }
--- api/chat-sv/Program.cs
+++ api/chat-sv/Program.cs
@@ -1,5 +1,9 @@
+using System.Security.Cryptography;
+using System.Text;
 using chat_sv.Controllers;
 using chat_sv.Hubs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
 using Newtonsoft.Json.Serialization;
 
@@ -29,8 +33,32 @@ builder.Services.AddControllers()
     });
 builder.Services.AddSingleton<IMongoClient>(sp => new MongoClient(configuration["MongoSetting:ConnectionString"]));
 builder.Services.AddSignalR();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = configuration["Jwt:Issuer"],            // Replace with your issuer
+        ValidAudience = configuration["Jwt:Audience"],        // Replace with your audience
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Secret"]!))  // Replace with your secret key
+    };
+});
 
 var app = builder.Build();
+
+// Inside your user creation logic
+string password = "user_password"; // Replace with the actual user's password
+byte[] passwordHash, passwordSalt;
+
+PasswordHasher.CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
 using (var scope = app.Services.CreateAsyncScope())
 {
@@ -47,12 +75,18 @@ using (var scope = app.Services.CreateAsyncScope())
             new User{
                 Id="c13cf23b-59c8-490d-8e8b-93c1988e203b",
                 Name="user 1",
-                Avatar="https://cdn.dribbble.com/users/3841177/screenshots/11950347/cartoon-avatar_2020__8_circle.png"
+                Username="user1",
+                Avatar="https://cdn.dribbble.com/users/3841177/screenshots/11950347/cartoon-avatar_2020__8_circle.png",
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
             },
             new User{
                 Id="29e7066b-0c42-48de-a835-1a103fd1dade",
                 Name="user 2",
-                Avatar="https://www.gamer-hub.io/static/img/team/sam.png"
+                Username="user2",
+                Avatar="https://www.gamer-hub.io/static/img/team/sam.png",
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
             },
         });
     }
@@ -73,3 +107,15 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapHub<ChatHub>("/hub/chat");
 app.Run();
+
+public class PasswordHasher
+{
+    public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
